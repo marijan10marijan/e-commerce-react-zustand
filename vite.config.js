47f5d87b2c035b5ce46165b5e4d446d04a2bcc1d
marijan10marijan <mmarijan39@gmail.com>
2024-11-18 12:29:18 +0100

    new changes

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Omogućuje pristup s vanjskih uređaja
-    port: 3000, // Možeš odabrati bilo koji slobodan port
-  },
+  base: "/e-commerce-react-zustand/",
 });
